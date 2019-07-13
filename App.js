db2ed8a54e8a573527ec907d30cd2ed18f9a2fa0
현태수 <qvil@op.gg>
2019-07-14 00:14:03 +0900

    Add loadResourcesAsync, icon, splash image

--- App.js
+++ App.js
@@ -1,22 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { AppLoading } from "expo";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
 import { createAppContainer } from "react-navigation";
 import AppNavigator from "./screens/AppNavigator";
-// import AppNavigator from "./screens/AppNavigator";
 
 const AppContainer = createAppContainer(AppNavigator);
 
-function App() {
-  return <AppContainer />;
+function App(props) {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+  if (!isLoadingComplete && !props.skipLoadingScreen) {
+    return (
+      <AppLoading
+        startAsync={loadResourcesAsync}
+        onError={handleLoadingError}
+        onFinish={() => handleFinishLoading(setLoadingComplete)}
+      />
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+        <AppContainer />
+      </View>
+    );
+  }
 }
 
-App.navigationOptions = props => ({
-  title: "Home",
-  headerStyle: {
-    backgroundColor: "#f4511e"
-  },
-  headerTintColor: "#fff",
-  headerTitleStyle: {
-    fontWeight: "bold"
+async function loadResourcesAsync() {
+  await Font.loadAsync({
+    Roboto: require("native-base/Fonts/Roboto.ttf"),
+    Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+    ...Ionicons.font
+  });
+  // await Promise.all([
+  //   Asset.loadAsync([
+  //     require("./assets/images/robot-dev.png"),
+  //     require("./assets/images/robot-prod.png")
+  //   ]),
+  //   Font.loadAsync({
+  //     Roboto: require("native-base/Fonts/Roboto.ttf"),
+  //     Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+  //     ...Ionicons.font
+  //     // We include SpaceMono because we use it in HomeScreen.js. Feel free to
+  //     // remove this if you are not using it in your app
+  //     // "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf")
+  //   })
+  // ]);
+}
+
+function handleLoadingError(error) {
+  // In this case, you might want to report the error to your error reporting
+  // service, for example Sentry
+  console.warn(error);
+}
+
+function handleFinishLoading(setLoadingComplete) {
+  setLoadingComplete(true);
+}
+
+// App.navigationOptions = props => ({
+//   title: "Home",
+//   headerStyle: {
+//     backgroundColor: "#f4511e"
+//   },
+//   headerTintColor: "#fff",
+//   headerTitleStyle: {
+//     fontWeight: "bold"
+//   }
+// });
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
   }
 });
 
