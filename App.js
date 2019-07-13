8a1ecce182ab618042bc9e3fde9394abeb7bcb2e
현태수 <qvil@op.gg>
2019-07-13 12:51:57 +0900

    Success CameraExample

--- App.js
+++ App.js
@@ -1,19 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import CameraExample from "./components/CameraExample";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <CameraExample />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
