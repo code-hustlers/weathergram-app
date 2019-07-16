ba1c48742f00211f010eca5d43345e8fd26dd559
현태수 <qvil@op.gg>
2019-07-16 22:47:14 +0900

    New env concept - using JS

---
+++ env.js
@@ -0,0 +1,5 @@
+const env = {
+  API_URL: "http://13.125.51.65:8080"
+};
+
+export default env;
--- screens/CameraExample.js
+++ screens/CameraExample.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, View, TouchableOpacity } from "react-native";
 import * as Permissions from "expo-permissions";
 import { Camera } from "expo-camera";
+import env from "../env";
 
 export default class CameraExample extends React.Component {
   state = {
@@ -30,7 +31,7 @@ export default class CameraExample extends React.Component {
           alert("onPictureSaved Success!");
         }
       });
-      fetch(`${process.env.API_URL}/photo`, {
+      fetch(`${env.API_URL}/photo`, {
         method: "POST",
         headers: {
           Accept: "application/json",
